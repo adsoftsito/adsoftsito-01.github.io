65c4a6c8f53662bf0018551e55aecdf3b07926a0
adsoftsito <adsoft@live.com.mx>
2022-01-08 02:09:28 -0600

    add cfdi component

---
+++ src/app/cfdi/cfdi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cfdi',
+  templateUrl: './cfdi.component.html',
+  styleUrls: ['./cfdi.component.css']
+})
+export class CfdiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
