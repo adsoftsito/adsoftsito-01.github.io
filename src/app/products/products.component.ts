1349464dd227c0dd7b03e55add4250c7c4175636
AizawaCode <leonardo.m2349@gmail.com>
2022-01-28 16:50:10 -0600

    reordenado de inputs y agregada ventana de eliminar

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import Swal from 'sweetalert2';
 import { GraphqlProductsService} from '../../services/graphql.products.service';
 import { StorageService } from "../../services/storage.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-products',
@@ -15,6 +17,7 @@ export class ProductsComponent implements OnInit, OnDestroy {
   user: string;
   token: string;
   valor: string;
+  private router: Router;
 
   constructor(private graphqlProductsService: GraphqlProductsService,
               private storageService : StorageService
@@ -47,11 +50,49 @@ export class ProductsComponent implements OnInit, OnDestroy {
   }
 
   deleteThisProduct(){
-   
+
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    
+    swalWithBootstrapButtons.fire({
+      title: '¿Estas seguro que quieres eliminar este producto?',
+      text: "No podrás deshacer los cambios.",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, eliminar',
+      cancelButtonText: 'No, cancelar',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire(
+          'Eliminado',
+          'Producto eliminado correctamente',
+          'success'
+        )
+
+        
+
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire(
+          'Cancelado',
+          'Operración cancelada',
+          'error'
+        )
+      }
+    })
   }
 
   updateThisProduct(){
-
+    console.log('redireccionado a productos');
+    this.router.navigate(["/admin/admin/new-product"]);
   }
 
   ngOnDestroy() {
