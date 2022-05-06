db8f8b691564d35697ce82ba6726389472832efd
adsoftsito <adsoft@live.com.mx>
2022-05-06 13:12:24 -0500

    init lineas

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,108 +1,3 @@
-<<<<<<< HEAD
-import { Routes } from '@angular/router';
-
-import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
-import { NewUserComponent } from '../../new-user/new-user.component';
-import { ProductsComponent } from '../../products/products.component';
-import { LoginComponent } from '../../login/login.component';
-import { NewProductComponent } from '../../new-product/new-product.component';
-import { CarritocomprasComponent } from '../../carritocompras/carritocompras.component';
-import { FacturacionComponent } from '../../facturacion/facturacion.component';
-import { ClientesComponent } from '../../clientes/clientes.component';
-import { ConfiguracionComponent } from '../../configuracion/configuracion.component';
-import { NewClientComponent } from '../../new-client/new-client.component';
-import { LogoutComponent } from '../../logout/logout.component';
-import { CfdiComponent } from '../../cfdi/cfdi.component';
-import { MarcasComponent } from '../../marcas/marcas.component';
-import { LineasComponent } from '../../lineas/lineas.component';
-import { ListasComponent } from '../../listas/listas.component';
-import { ListascompraComponent } from '../../listascompra/listascompra.component';
-
-import { ProveedoresComponent } from '../../proveedores/proveedores.component';
-import { ComprasComponent } from '../../compras/compras.component';
-
-export const AdminLayoutRoutes: Routes = [
-    // {
-    //   path: '',
-    //   children: [ {
-    //     path: 'dashboard',
-    //     component: DashboardComponent
-    // }]}, {
-    // path: '',
-    // children: [ {
-    //   path: 'userprofile',
-    //   component: UserProfileComponent
-    // }]
-    // }, {
-    //   path: '',
-    //   children: [ {
-    //     path: 'icons',
-    //     component: IconsComponent
-    //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'notifications',
-    //         component: NotificationsComponent
-    //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'maps',
-    //         component: MapsComponent
-    //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'typography',
-    //         component: TypographyComponent
-    //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'upgrade',
-    //         component: UpgradeComponent
-    //     }]
-    // }
-    { path: 'admin/dashboard',     component: DashboardComponent },
-    { path: 'admin/facturacion',   component: FacturacionComponent },
-    { path: 'admin/productos',     component: ProductsComponent },
-    { path: 'admin/clientes',      component: ClientesComponent },
-    { path: 'admin/marcas',      component: MarcasComponent },
-    { path: 'admin/lineas',      component: LineasComponent },
-    { path: 'admin/listas',      component: ListasComponent },
-    { path: 'admin/listascompra',      component: ListascompraComponent },
-
-    { path: 'admin/proveedores',      component: ProveedoresComponent },
-    { path: 'admin/compras',      component: ComprasComponent },
-    { path: 'admin/configuracion', component: ConfiguracionComponent },
-    { path: 'admin/new-product',   component: NewProductComponent },
-    { path: 'admin/new-product/:id',   component: NewProductComponent },
-    { path: 'admin/new-client',    component: NewClientComponent },
-    
-    { path: 'admin/logout',        component: LogoutComponent },
-    { path: 'admin/cfdi/:id',          component: CfdiComponent },
-
-
-
-    //{ path: 'new-user',       component: NewUserComponent },
-    //{ path: 'login',          component: LoginComponent },
-    //{ path: 'carrito',        component: CarritocomprasComponent },
-    //{ path: 'admin/table-list',     component: TableListComponent },
-    //{ path: 'typography',     component: TypographyComponent },
-    //{ path: 'icons',          component: IconsComponent },
-    //{ path: 'maps',           component: MapsComponent },
-    //{ path: 'notifications',  component: NotificationsComponent },
-    //{ path: 'upgrade',        component: UpgradeComponent },
-];
-=======
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../../dashboard/dashboard.component';
@@ -188,7 +83,10 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'admin/proveedores',      component: ProveedoresComponent },
     { path: 'admin/compras',      component: ComprasComponent },
     { path: 'admin/configuracion', component: ConfiguracionComponent },
+
     { path: 'admin/new-product',   component: NewProductComponent },
+    { path: 'admin/new-product/:id',   component: NewProductComponent },
+
     { path: 'admin/new-client',    component: NewClientComponent },
     { path: 'admin/new-marca/:id',    component: NewMarcaComponent },
 
@@ -207,4 +105,3 @@ export const AdminLayoutRoutes: Routes = [
     //{ path: 'notifications',  component: NotificationsComponent },
     //{ path: 'upgrade',        component: UpgradeComponent },
 ];
->>>>>>> marcas
--- src/app/new-product/new-product.component.spec.ts
+++ src/app/new-product/new-product.component.spec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewProductComponent } from './new-product.component';
@@ -25,30 +24,3 @@ describe('NewProductComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-=======
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { NewProductComponent } from './new-product.component';
-
-describe('NewProductComponent', () => {
-  let component: NewProductComponent;
-  let fixture: ComponentFixture<NewProductComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NewProductComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NewProductComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
->>>>>>> marcas
