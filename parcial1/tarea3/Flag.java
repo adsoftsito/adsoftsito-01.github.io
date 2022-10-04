6fe4326d5c09dd2ac32faf4d8694a434bbdc9ec8
adsoftsito <adsoft@live.com.mx>
2022-10-04 18:31:52 -0500

    se agrego range.java

---
+++ parcial1/tarea3/Flag.java
@@ -0,0 +1,15 @@
+/*
+ * Flag.java
+ * Author: adsoft
+ * Date: Oct 4, 2022
+ * Location: Ixtac, Ver. Mexico
+ */
+
+class Flag {  // name of Program
+  public static void main(String[] args) {  // main method
+    	
+    boolean flag = false;
+    System.out.println("Valor de flag: ");          // define a boolean var
+    System.out.println(!!flag);    // prints true
+  }
+}
